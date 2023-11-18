f2a5fa0af7cc268d32e66185265eee42bb2b0b77
Fabricio Flores <me@fabricioflores.se>
2023-11-18 20:17:57 +0100

    Category entity

---
+++ src/main/java/se/fabricioflores/springrestapi/model/Category.java
@@ -0,0 +1,80 @@
+package se.fabricioflores.springrestapi.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String symbol;
+
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "category_location",
+            joinColumns = @JoinColumn(
+                    name = "category_id",
+                    foreignKey = @ForeignKey(name = "fk_category")
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "location_id",
+                    foreignKey = @ForeignKey(name = "fk_location")
+            )
+    )
+    private Set<Location> locations = new LinkedHashSet<>();
+
+    public Category() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Location> getLocations() {
+        return locations;
+    }
+
+    public void setLocations(Set<Location> locations) {
+        this.locations = locations;
+    }
+}
