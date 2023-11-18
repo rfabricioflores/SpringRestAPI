463b2bf764de4bc7e62d9c3167d4256bc9e8316a
Fabricio Flores <me@fabricioflores.se>
2023-11-18 20:15:26 +0100

    Changed base path to /api

--- src/main/java/se/fabricioflores/springrestapi/SpringRestApiApplication.java
+++ src/main/java/se/fabricioflores/springrestapi/SpringRestApiApplication.java
@@ -2,11 +2,19 @@ package se.fabricioflores.springrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRestApiApplication.class, args);
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setContextPath("/api");
     }
 }
