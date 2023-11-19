80eaa9b5bd5e23db9d4545f6769f4b10f0c1ecaa
Fabricio Flores <me@fabricioflores.se>
2023-11-19 17:37:18 +0100

    Updated category service

--- src/main/java/se/fabricioflores/springrestapi/service/CategoryService.java
+++ src/main/java/se/fabricioflores/springrestapi/service/CategoryService.java
@@ -3,6 +3,7 @@ package se.fabricioflores.springrestapi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.fabricioflores.springrestapi.model.Category;
+import se.fabricioflores.springrestapi.projection.CategoryDetail;
 import se.fabricioflores.springrestapi.repo.CategoryRepo;
 
 import java.util.List;
@@ -17,12 +18,12 @@ public class CategoryService {
         this.categoryRepo = categoryRepo;
     }
 
-    public List<Category> getAllCategories() {
-        return categoryRepo.findAll();
+    public List<CategoryDetail> getAllCategories() {
+        return categoryRepo.findAllBy();
     }
 
-    public Optional<Category> getCategoryById(Long categoryId) {
-        return categoryRepo.findById(categoryId);
+    public Optional<CategoryDetail> getCategoryById(Long id) {
+        return categoryRepo.findOneById(id);
     }
 
     public Category addCategory(Category category) {
