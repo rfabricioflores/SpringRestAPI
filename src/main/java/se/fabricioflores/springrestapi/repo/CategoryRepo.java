85e7a1ece3edbe1c313af4748634d89a96bc6f77
Fabricio Flores <me@fabricioflores.se>
2023-11-19 17:32:34 +0100

    Updated repo to retrieve list of category detail entities and one specific category entity

--- src/main/java/se/fabricioflores/springrestapi/repo/CategoryRepo.java
+++ src/main/java/se/fabricioflores/springrestapi/repo/CategoryRepo.java
@@ -2,5 +2,12 @@ package se.fabricioflores.springrestapi.repo;
 
 import org.springframework.data.repository.ListCrudRepository;
 import se.fabricioflores.springrestapi.model.Category;
+import se.fabricioflores.springrestapi.projection.CategoryDetail;
 
-public interface CategoryRepo extends ListCrudRepository<Category, Long> { }
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepo extends ListCrudRepository<Category, Long> {
+    List<CategoryDetail> findAllBy();
+    Optional<CategoryDetail> findOneById(Long id);
+}
