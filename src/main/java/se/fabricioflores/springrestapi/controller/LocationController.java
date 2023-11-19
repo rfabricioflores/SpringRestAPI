a86b1e1206f3892b2ddb8cafc6431cd10ac01ce5
Fabricio Flores <me@fabricioflores.se>
2023-11-19 17:42:17 +0100

    Location controller template

---
+++ src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
@@ -0,0 +1,9 @@
+package se.fabricioflores.springrestapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/location", "/location/"})
+public class LocationController {
+}
