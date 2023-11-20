4eb1befc2c4c914ecd50666ea5184ed7b115a2c0
Fabricio Flores <me@fabricioflores.se>
2023-11-20 20:08:02 +0100

    Basic location service for retrieval and creation of locations

--- src/main/java/se/fabricioflores/springrestapi/service/LocationService.java
+++ src/main/java/se/fabricioflores/springrestapi/service/LocationService.java
@@ -1,9 +1,11 @@
-
 package se.fabricioflores.springrestapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.fabricioflores.springrestapi.model.Location;
 import se.fabricioflores.springrestapi.repo.LocationRepo;
+
+import java.util.List;
 
 @Service
 public class LocationService {
@@ -12,5 +14,13 @@ public class LocationService {
     @Autowired
     public LocationService(LocationRepo locationRepo) {
         this.locationRepo = locationRepo;
+    }
+
+    public List<Location> getAllLocations() {
+        return locationRepo.findAll();
+    }
+
+    public Location addLocation(Location location) {
+        return locationRepo.save(location);
     }
 }
