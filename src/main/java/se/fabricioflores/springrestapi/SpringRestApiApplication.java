546cc9429ac2aafac40d15d9568bda4433a2436e
Fabricio Flores <me@fabricioflores.se>
2023-11-20 19:38:48 +0100

    Configuration for geo latte (spatial data)

--- src/main/java/se/fabricioflores/springrestapi/SpringRestApiApplication.java
+++ src/main/java/se/fabricioflores/springrestapi/SpringRestApiApplication.java
@@ -1,10 +1,15 @@
 package se.fabricioflores.springrestapi;
 
+import org.geolatte.geom.G2D;
+import org.geolatte.geom.crs.CoordinateReferenceSystem;
+import org.geolatte.geom.json.GeolatteGeomModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+
+import static org.geolatte.geom.crs.CoordinateReferenceSystems.WGS84;
 
 @SpringBootApplication
 public class SpringRestApiApplication {
@@ -13,8 +18,16 @@ public class SpringRestApiApplication {
         SpringApplication.run(SpringRestApiApplication.class, args);
     }
 
+    // ** Base path
     @Bean
     public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
         return factory -> factory.setContextPath("/api");
+    }
+
+    // ** Json serializer for geo latte objects
+    @Bean
+    public GeolatteGeomModule geolatteGeomModule() {
+        CoordinateReferenceSystem<G2D> crs = WGS84;
+        return new GeolatteGeomModule(crs);
     }
 }
