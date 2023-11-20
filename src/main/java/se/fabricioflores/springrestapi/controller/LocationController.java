9313309857fa19081ff5fad68608e00b5d28c7a5
Fabricio Flores <me@fabricioflores.se>
2023-11-20 20:12:36 +0100

    Basic location controller

--- src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
+++ src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
@@ -1,9 +1,23 @@
 package se.fabricioflores.springrestapi.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import se.fabricioflores.springrestapi.service.LocationService;
 
 @RestController
 @RequestMapping({"/location", "/location/"})
 public class LocationController {
+    private final LocationService locationService;
+
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Object> getLocations() {
+        var locations = locationService.getAllLocations();
+        return ResponseEntity.ok(locations);
+    }
 }
