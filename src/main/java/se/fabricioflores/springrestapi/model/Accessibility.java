9652e9dc1a4109d49bcfb2a6e12e68a1832b0a4c
Fabricio Flores <me@fabricioflores.se>
2023-11-21 20:50:27 +0100

    Added jackson de/serialization for this enum

--- src/main/java/se/fabricioflores/springrestapi/model/Accessibility.java
+++ src/main/java/se/fabricioflores/springrestapi/model/Accessibility.java
@@ -1,7 +1,15 @@
 package se.fabricioflores.springrestapi.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Accessibility {
     // ** For storing purposes this enum is modified by its attribute converter
     PUBLIC,
     PRIVATE;
+
+    // ** Jackson de/serialization
+    @JsonValue
+    public String getValue() {
+        return this.toString().toLowerCase();
+    }
 }
