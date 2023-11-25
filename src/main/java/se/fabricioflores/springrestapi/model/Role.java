33aee7c2edda645ea7a100d3a791b80c62e5bbf6
Fabricio Flores <me@fabricioflores.se>
2023-11-25 14:41:01 +0100

    Update role entity

--- src/main/java/se/fabricioflores/springrestapi/model/Role.java
+++ src/main/java/se/fabricioflores/springrestapi/model/Role.java
@@ -18,5 +18,22 @@ public class Role {
     @Column(nullable = false, updatable = false, unique = true)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
