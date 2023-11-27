04943b0f261b8433d42f3917ed4bd99375188091
Fabricio Flores <me@fabricioflores.se>
2023-11-27 21:12:36 +0100

    User service implementation

---
+++ src/main/java/se/fabricioflores/springrestapi/service/UserService.java
@@ -0,0 +1,94 @@
+package se.fabricioflores.springrestapi.service;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import se.fabricioflores.springrestapi.dto.AddUserReq;
+import se.fabricioflores.springrestapi.dto.UserRegisterReq;
+import se.fabricioflores.springrestapi.model.Role;
+import se.fabricioflores.springrestapi.model.User;
+import se.fabricioflores.springrestapi.repo.RoleRepo;
+import se.fabricioflores.springrestapi.repo.UserRepo;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements IUserService {
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepo userRepo, RoleRepo roleRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // ** This is mainly used by admins
+    @Override
+    public User save(@Valid AddUserReq addUserReq) throws RuntimeException {
+        var optUser = userRepo.findOneByUsername(addUserReq.username());
+        if(optUser.isPresent()) throw new RuntimeException("User already exists");
+
+        User user = new User();
+        user.setUsername(addUserReq.username());
+        user.setPassword(passwordEncoder.encode(addUserReq.password()));
+
+        Set<Role> roles = new HashSet<>();
+        System.out.println("hash set");
+
+        addUserReq.roles().forEach(r -> {
+            var optRole = roleRepo.findRoleByName(r);
+            optRole.ifPresent(roles::add);
+        });
+
+        user.setRoles(roles);
+
+        return userRepo.save(user);
+    }
+
+    // ** Register new users
+    @Override
+    public User registerUser(@Valid UserRegisterReq userRegisterReq) {
+        User user = new User();
+        user.setUsername(userRegisterReq.username());
+        user.setPassword(passwordEncoder.encode(userRegisterReq.password()));
+
+        Role role = roleRepo.findRoleByName("USER").orElseThrow();
+
+        user.setRoles(Set.of(role));
+
+        return userRepo.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo
+                .findOneByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                mapRolesToAuthorities(user.getRoles())
+                );
+    }
+
+    // ** Convert role entities to authorities
+    public Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles){
+        return roles
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+}
