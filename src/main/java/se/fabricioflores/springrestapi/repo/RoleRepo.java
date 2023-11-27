f36bfc4ecd4b356836f30afe9094f852bf059cb3
Fabricio Flores <me@fabricioflores.se>
2023-11-27 20:53:15 +0100

    Find role by name

--- src/main/java/se/fabricioflores/springrestapi/repo/RoleRepo.java
+++ src/main/java/se/fabricioflores/springrestapi/repo/RoleRepo.java
@@ -3,5 +3,8 @@ package se.fabricioflores.springrestapi.repo;
 import org.springframework.data.repository.ListCrudRepository;
 import se.fabricioflores.springrestapi.model.Role;
 
+import java.util.Optional;
+
 public interface RoleRepo extends ListCrudRepository<Role, Long> {
+    Optional<Role> findRoleByName(String name);
 }
