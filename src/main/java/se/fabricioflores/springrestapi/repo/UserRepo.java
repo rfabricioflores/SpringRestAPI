8598803ac064d1c218629ea62affa3a99e0fb943
Fabricio Flores <me@fabricioflores.se>
2023-11-27 20:53:41 +0100

    Find user by username method

--- src/main/java/se/fabricioflores/springrestapi/repo/UserRepo.java
+++ src/main/java/se/fabricioflores/springrestapi/repo/UserRepo.java
@@ -3,5 +3,8 @@ package se.fabricioflores.springrestapi.repo;
 import org.springframework.data.repository.ListCrudRepository;
 import se.fabricioflores.springrestapi.model.User;
 
+import java.util.Optional;
+
 public interface UserRepo extends ListCrudRepository<User, Long> {
+    Optional<User> findOneByUsername(String username);
 }
