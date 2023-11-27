3c729c54e0960788a563386db2583c64bdddff76
Fabricio Flores <me@fabricioflores.se>
2023-11-27 21:13:56 +0100

    Security configuration

--- src/main/java/se/fabricioflores/springrestapi/config/SecurityConfig.java
+++ src/main/java/se/fabricioflores/springrestapi/config/SecurityConfig.java
@@ -1,14 +1,56 @@
 package se.fabricioflores.springrestapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import se.fabricioflores.springrestapi.service.IUserService;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final IUserService userService;
+    private final JwtFilter jwtFilter;
+
+    @Autowired
+    public SecurityConfig(@Lazy IUserService userService, JwtFilter jwtFilter) {
+        this.userService = userService;
+        this.jwtFilter = jwtFilter;
+    }
+
+    // ** Tells the auth provider to use our custom user service and password encoder for authentication
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        var auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // ** Password encoder using BCrypt
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     // ** Base path e.g /api/ should NOT be added to the request matchers
     @Bean
@@ -16,12 +58,14 @@ public class SecurityConfig {
         http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/login", "/auth/register").permitAll()
                         .requestMatchers("/categories", "/categories/").permitAll()
                         .requestMatchers("/location/**").authenticated()
-                        .anyRequest().denyAll()
-                );
+                        .requestMatchers("/auth/welcome").hasAnyAuthority("ADMIN", "USER")
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
