bcc0068fbc4299803dacb1ac5f98d276b26ae462
Fabrithegreat <32011023+MrFabri@users.noreply.github.com>
2023-12-03 09:36:49 +0100

    Updated custom user service interface

--- src/main/java/se/fabricioflores/springrestapi/service/IUserService.java
+++ src/main/java/se/fabricioflores/springrestapi/service/IUserService.java
@@ -5,7 +5,10 @@ import se.fabricioflores.springrestapi.dto.AddUserReq;
 import se.fabricioflores.springrestapi.dto.UserRegisterReq;
 import se.fabricioflores.springrestapi.model.User;
 
+import java.util.Optional;
+
 public interface IUserService extends UserDetailsService {
-    User save(AddUserReq addUserReq) throws RuntimeException;
+    User createUser(AddUserReq addUserReq) throws RuntimeException;
     User registerUser(UserRegisterReq userRegisterReq);
+    Optional<User> getUser(String username);
 }
