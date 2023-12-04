e03c4234fd0d8b790762e374ab0c92b14424f963
Fabrithegreat <32011023+MrFabri@users.noreply.github.com>
2023-12-04 10:57:42 +0100

    Updated location public endpoint with query param for category (optional)

--- src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
+++ src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
@@ -7,8 +7,10 @@ import se.fabricioflores.springrestapi.dto.AddLocationReq;
 import se.fabricioflores.springrestapi.service.IUserService;
 import se.fabricioflores.springrestapi.service.LocationService;
 
+import java.util.Optional;
+
 @RestController
-@RequestMapping({"/location", "/location/"})
+@RequestMapping({"/locations", "/locations/"})
 public class LocationController {
     private final LocationService locationService;
     private final IUserService userService;
@@ -29,10 +31,29 @@ public class LocationController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getLocation(@PathVariable(name = "id") Long id) {
-        var location =  locationService.getLocation(id).orElseThrow(() -> new RuntimeException("Location with id" + id + " does not exists"));
+        var location =  locationService.getLocation(id).orElseThrow(() -> new RuntimeException("Location does not exist"));
         return ResponseEntity.ok().body(location);
     }
 
+    // ** Public locations
+    @GetMapping("/public")
+    public ResponseEntity<Object> getPublicLocations(
+            @RequestParam(name = "category") Optional<Long> categoryId
+    ) {
+        if(categoryId.isPresent()) {
+            return ResponseEntity.ok().body(locationService.getPublicLocationsByCategory(categoryId.get()));
+        } else {
+            return ResponseEntity.ok().body(locationService.getPublicLocations());
+        }
+    }
+
+    @GetMapping("/public/{locationId}")
+    public ResponseEntity<Object> getPublicLocation(@PathVariable(name = "locationId") Long id) {
+        var location = locationService.getPublicLocation(id).orElseThrow(() -> new RuntimeException("Location does not exist"));
+        return ResponseEntity.ok().body(location);
+    }
+
+    // ** Create a new location
     @PostMapping
     public ResponseEntity<Object> addLocation(@RequestBody AddLocationReq body) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
