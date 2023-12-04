7ddbb192307a27811c8ab069e7a9ff6c16182449
Fabricio Flores <me@fabricioflores.se>
2023-12-04 11:05:05 +0100

    Lombok refactor

--- src/main/java/se/fabricioflores/springrestapi/model/User.java
+++ src/main/java/se/fabricioflores/springrestapi/model/User.java
@@ -1,6 +1,9 @@
 package se.fabricioflores.springrestapi.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
@@ -11,6 +14,8 @@ import java.util.Set;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Setter
 public class User implements Serializable {
 
     public User() {
@@ -19,7 +24,7 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(nullable = false, updatable = false, unique = true)
-
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @Column(unique = true, nullable = false)
@@ -42,37 +47,6 @@ public class User implements Serializable {
     )
     private Set<Role> roles = new HashSet<>();
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 
     @Override
     public final boolean equals(Object o) {
