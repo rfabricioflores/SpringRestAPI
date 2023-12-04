ec75603385852d95b68d4b9718091dc4521d1c02
Fabrithegreat <32011023+MrFabri@users.noreply.github.com>
2023-12-04 07:33:59 +0100

    Lombok refactor

--- src/main/java/se/fabricioflores/springrestapi/model/Location.java
+++ src/main/java/se/fabricioflores/springrestapi/model/Location.java
@@ -9,6 +9,10 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 import org.geolatte.geom.G2D;
 import org.geolatte.geom.Point;
 
@@ -23,6 +27,8 @@ import org.hibernate.proxy.HibernateProxy;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Setter
 public class Location implements Serializable {
 
     public Location() {
@@ -45,17 +51,24 @@ public class Location implements Serializable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(nullable = false, updatable = false, unique = true)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @Column(nullable = false)
     private String name;
 
-    @ManyToMany(mappedBy = "locations", fetch = FetchType.EAGER)
-    private Set<Category> categories = new LinkedHashSet<>();
+    private Accessibility accessibility = Accessibility.PUBLIC;
 
     private Long userId;
 
-    private Accessibility accessibility = Accessibility.PUBLIC;
+    private String description;
+
+    @ManyToMany(mappedBy = "locations", fetch = FetchType.EAGER)
+    private Set<Category> categories = new LinkedHashSet<>();
+
+    @JsonSerialize(using = Point2DSerializer.class)
+    @JsonDeserialize(using = Point2DDeserializer.class)
+    private Point<G2D> coordinate;
 
     @UpdateTimestamp
     @Column(nullable = false)
@@ -65,83 +78,6 @@ public class Location implements Serializable {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    private String description;
-
-    @JsonSerialize(using = Point2DSerializer.class)
-    @JsonDeserialize(using = Point2DDeserializer.class)
-    private Point<G2D> coordinate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(Set<Category> categories) {
-        this.categories = categories;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Accessibility getAccessibility() {
-        return accessibility;
-    }
-
-    public void setAccessibility(Accessibility accessibility) {
-        this.accessibility = accessibility;
-    }
-
-    public LocalDateTime getEditedAt() {
-        return editedAt;
-    }
-
-    public void setEditedAt(LocalDateTime editedAt) {
-        this.editedAt = editedAt;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Point<G2D> getCoordinate() {
-        return coordinate;
-    }
-
-    public void setCoordinate(Point<G2D> coordinate) {
-        this.coordinate = coordinate;
-    }
 
     @Override
     public final boolean equals(Object o) {
