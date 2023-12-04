3a4ac58f3284504e404d6ba501f24bb3c6ebe422
Fabricio Flores <me@fabricioflores.se>
2023-12-04 22:46:19 +0100

    Updated controller

--- src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
+++ src/main/java/se/fabricioflores/springrestapi/controller/LocationController.java
@@ -1,5 +1,9 @@
 package se.fabricioflores.springrestapi.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import org.geolatte.geom.G2D;
+import org.geolatte.geom.Point;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +12,10 @@ import se.fabricioflores.springrestapi.service.IUserService;
 import se.fabricioflores.springrestapi.service.LocationService;
 
 import java.util.Optional;
+
+import static org.geolatte.geom.builder.DSL.g;
+import static org.geolatte.geom.builder.DSL.point;
+import static org.geolatte.geom.crs.CoordinateReferenceSystems.WGS84;
 
 @RestController
 @RequestMapping({"/locations", "/locations/"})
@@ -69,6 +77,21 @@ public class LocationController {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         var user = userService.getUser(username).orElseThrow();
         var locations = locationService.getLocationsOfUser(user.getId());
+        return ResponseEntity.ok().body(locations);
+    }
+
+    // ** Near by locations
+    @GetMapping("/nearby")
+    public ResponseEntity<Object> getLocationsNearby(
+            @RequestParam @Valid @NotNull @NotEmpty Double lat,
+            @RequestParam @Valid @NotNull @NotEmpty Double lon
+    ) {
+        if(lat > 90 || lat < -90) throw new IllegalArgumentException("Not valid latitude");
+        if(lon > 180 || lon < -180) throw new IllegalArgumentException("Not valid longitude");
+
+        Point<G2D> coordinate = point(WGS84, g(lon, lat));
+
+        var locations = locationService.getLocationsWithinRadiusFromCoordinate(coordinate, 10000.0);
         return ResponseEntity.ok().body(locations);
     }
 
