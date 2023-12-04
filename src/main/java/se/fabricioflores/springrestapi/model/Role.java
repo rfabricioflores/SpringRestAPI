073c356739bdd3daefe6a1aa87749903fbeb7755
Fabricio Flores <me@fabricioflores.se>
2023-12-04 11:04:58 +0100

    Lombok refactor

--- src/main/java/se/fabricioflores/springrestapi/model/Role.java
+++ src/main/java/se/fabricioflores/springrestapi/model/Role.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.proxy.HibernateProxy;
 
 import java.io.Serializable;
@@ -12,6 +15,8 @@ import java.util.Objects;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
+@Setter
 public class Role implements Serializable {
 
     public Role() {
@@ -24,26 +29,12 @@ public class Role implements Serializable {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @Column(nullable = false, updatable = false, unique = true)
+    @Setter(AccessLevel.NONE)
     private Long id;
 
     @Column(nullable = false, unique = true)
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public final boolean equals(Object o) {
