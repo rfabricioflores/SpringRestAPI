c4c45b55754f36ef7f816fa48fae61dbe48bb221
Fabricio Flores <me@fabricioflores.se>
2023-12-05 21:18:07 +0100

    Finished updateLocation and deleteLocation methods

--- src/main/java/se/fabricioflores/springrestapi/service/LocationService.java
+++ src/main/java/se/fabricioflores/springrestapi/service/LocationService.java
@@ -1,11 +1,14 @@
 package se.fabricioflores.springrestapi.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.geolatte.geom.G2D;
 import org.geolatte.geom.Point;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import se.fabricioflores.springrestapi.dto.LocationDto;
+import se.fabricioflores.springrestapi.dto.LocationEditDto;
 import se.fabricioflores.springrestapi.model.Accessibility;
 import se.fabricioflores.springrestapi.model.Category;
 import se.fabricioflores.springrestapi.model.Location;
@@ -80,12 +83,57 @@ public class LocationService {
         return locationRepo.getLocationsWithinRadius(coordinate, radiusInMeters);
     }
 
-    public Location updateLocation(Long locationId) {
+    @Transactional
+    public Location updateLocation(LocationEditDto data, Long userId) {
+        Location location = locationRepo.findById(data.id()).orElseThrow();
 
-        return null;
+        if (!location.getUserId().equals(userId))
+            throw new AccessDeniedException("You don't have access to this location");
+
+        if (data.name() != null) location.setName(data.name());
+        if (data.accessibility() != null) location.setAccessibility(data.accessibility());
+        if (data.description() != null) location.setDescription(data.description());
+        if (data.coordinate() != null) location.setCoordinate(data.coordinate());
+
+        if (data.categories() != null) {
+            // Retrieve categories based on the provided category ids list
+            Set<Category> newCategories = new HashSet<>(categoryRepo.findAllById(data.categories()));
+
+            // Update bidirectional relationships
+            for (Category category : location.getCategories()) {
+                category.getLocations().remove(location);
+            }
+
+            // Clear location categories and update them with the new ones
+            location.getCategories().clear();
+            location.getCategories().addAll(newCategories);
+
+            // Update bidirectional relationships
+            for (Category category : newCategories) {
+                category.getLocations().add(location);
+            }
+        }
+
+        return locationRepo.save(location);
     }
 
-    public boolean deleteLocation() {
-        return false;
+    @Transactional
+    public void deleteLocation(Long locationId, Long userId) {
+        // Checks if locations exists
+        Location location = locationRepo.findById(locationId).orElseThrow(() -> new EntityNotFoundException("Not valid location id"));
+
+        // Check if the user logged in owns this location
+        if (!location.getUserId().equals(userId))
+            throw new AccessDeniedException("You don't have access to this location");
+
+        // Update bidirectional relationships
+        // This could have been achieved with CascadeType.REMOVE but i decided to do it this way
+        for (Category category : location.getCategories()) {
+            category.getLocations().remove(location);
+        }
+
+        location.getCategories().clear();
+
+        locationRepo.delete(location);
     }
 }
