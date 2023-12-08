b5ec515f2e8127c00a73dd51dcb79220d7e970b9
Fabricio Flores <me@fabricioflores.se>
2023-12-08 17:47:43 +0100

    Implemented spring retry and recover method

--- src/main/java/se/fabricioflores/springrestapi/service/GeoCodeService.java
+++ src/main/java/se/fabricioflores/springrestapi/service/GeoCodeService.java
@@ -4,8 +4,12 @@ import org.geolatte.geom.G2D;
 import org.geolatte.geom.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClientException;
 import se.fabricioflores.springrestapi.dto.GeoResponse;
 
 @Service
@@ -21,8 +25,12 @@ public class GeoCodeService {
         this.restClient = restClientBuilder.baseUrl(geoCodeUrl).build();
     }
 
+    @Retryable(
+            retryFor = RestClientException.class,
+            backoff = @Backoff(delay = 1000),
+            maxAttempts = 2
+    )
     public GeoResponse convertCoordinateToAdress(Point<G2D> coordinate) {
-        System.out.println("service");
         Double latitude = coordinate.getPosition().getLat();
         Double longitude = coordinate.getPosition().getLon();
 
@@ -32,4 +40,11 @@ public class GeoCodeService {
                 .retrieve()
                 .body(GeoResponse.class);
     }
+
+    @Recover
+    public GeoResponse recover(RestClientException exception, Point<G2D> ignoredCoordinate) {
+        System.out.println(exception.getMessage());
+        return new GeoResponse(null, "Geocode service is down, please try again later!");
+    }
+
 }
